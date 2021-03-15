50ed63c34933b07896c8804885d7746ce642bf39
luispguerreiro <luis.p.guerreiro@gmail.com>
2021-03-15 19:22:54 +0000

    tt

--- maisTestes/src/maisTestes/test.java
+++ maisTestes/src/maisTestes/test.java
@@ -2,10 +2,19 @@ package maisTestes;
 
 public class test {
 
-//João
+//João + Luis
 	public static void main(String[] args) {
 		System.out.println("Rui, deixa de ser do benfica!");
 
+		//Test+++
+		
+		
+		//1
+		
+		//2
+		
+		
+		
 	}
 
 }
