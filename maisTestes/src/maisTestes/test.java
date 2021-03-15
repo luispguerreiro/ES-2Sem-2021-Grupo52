cf444ad062ca50dcca87bca54530d957308fcf01
rfcfa-iscte <rfcfa@iscte-iul.pt>
2021-03-15 20:42:24 +0000

    Loc-Rui

--- maisTestes/src/maisTestes/test.java
+++ maisTestes/src/maisTestes/test.java
@@ -1,10 +1,30 @@
 package maisTestes;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class test {
 //rui
 //teste
-	public static void main(String[] args) {
+	
+	public static int Loc (String FileName) throws FileNotFoundException {
+		
+		File file = new File(FileName);
+		Scanner iterate = new Scanner (file);
+		int numLines = 0;
+		while (iterate.hasNext()) {
+			iterate.nextLine();
+			numLines ++;
+		}
+		
+		
+		return numLines; 
+		
+	}
+	public static void main(String[] args) throws FileNotFoundException {
 		System.out.println("Rui, deixa de ser do benfica!");
+		System.out.println(Loc("Loc.java"));
 
 	}
 
