cea3b3181105f47057d32524608d53330043cbaf
hmpms-iscte <hmpms@iscte-iul.pt>
2021-03-22 14:53:01 +0000

    add isgod Class

--- maisTestes/src/maisTestes/Rule.java
+++ maisTestes/src/maisTestes/Rule.java
@@ -97,8 +97,8 @@ public class Rule {
 		Threshold t = new Threshold("LOC_method", 100, Threshold.comparator.BIGGER, 50);
 		Threshold t1 = new Threshold("WMC_class", 50, Threshold.comparator.BIGGER, 50);
 		Threshold t2 = new Threshold("NOM_class", 30, Threshold.comparator.BIGGER, 50);
-		getThresholds().add(t);
-//		getThresholds().add(t1);
+//		getThresholds().add(t);
+		getThresholds().add(t1);
 		getThresholds().add(t2);
 //		operators.add(logicOperator.AND);
 		operators.add(logicOperator.OR);
