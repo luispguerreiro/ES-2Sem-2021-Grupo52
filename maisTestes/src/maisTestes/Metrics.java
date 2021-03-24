07cec502688596645eed9e9ff17beba34ac86a76
rfcfa-iscte <rfcfa@iscte-iul.pt>
2021-03-24 16:18:44 +0000

    locClass

--- maisTestes/src/maisTestes/Metrics.java
+++ maisTestes/src/maisTestes/Metrics.java
@@ -10,11 +10,14 @@ import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.AnnotationDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
-public class Metrics{
+public class Metrics {
 	// private static final String SRC_PATH =
 	// "C:\\Users\\r_f_g\\eclipse-workspace\\Es Teste\\src";
+	static int linhasClass;
+	static int linhasMethod;
 	private static final String FILE_PATH = "C:\\Users\\r_f_g\\eclipse-workspace\\JparsecTeste\\Snippet.java";
 	private static final String SRC_PATH = "C:\\Users\\r_f_g\\eclipse-workspace\\JparsecTeste";
 	private static CompilationUnit cu = null;
@@ -23,34 +26,42 @@ public class Metrics{
 		CompilationUnit cu = StaticJavaParser.parse(new File(FILE_PATH));
 		List<String> methodNamesLines = new ArrayList<>();
 		List<String> ClassLines = new ArrayList<>();
-		new Loc_Method().visit(cu, methodNamesLines);
-		new Loc_Class().visit(cu, ClassLines);
-		
- 
+		//new Loc_Method().visit(cu, methodNamesLines);
+		locClass(cu);
+
 	}
-	
-	
+
+	private static void locClass(CompilationUnit cu) {
+		List<TypeDeclaration> nodes = cu.findAll(TypeDeclaration.class);
+
+		for (TypeDeclaration n : nodes) {
+			if (n.isClassOrInterfaceDeclaration()) {
+				int inicio=n.getBegin().get().line;
+				int fim = n.getEnd().get().line;
+				linhasClass = fim-inicio;
+				System.out.println(linhasClass);
+			}
+			
+	}
+
+		
+	}
+
 	private static class Loc_Method extends VoidVisitorAdapter<List<String>> {
 		@Override
 		public void visit(MethodDeclaration m, List<String> collector) {
 			super.visit(m, collector);
 			int inicio = m.getBegin().get().line;
 			int fim = m.getEnd().get().line;
-			int linhas = fim - inicio;
-			System.out.println(m.getNameAsString());
-			System.out.println(Integer.toString(linhas));
+			int linhasMethod = fim - inicio;
+			// System.out.println(m.getNameAsString());
+			System.out.println(Integer.toString(linhasMethod));
 			collector.add(m.getNameAsString());
-			collector.add(Integer.toString(linhas));
+			collector.add(Integer.toString(linhasMethod));
 
 		}
-		
+
 	}
-	
-	private static class Loc_Class extends VoidVisitorAdapter<List<String>> {
-		@Override
-		public void visit(AnnotationDeclaration n, List<String> arg) {
-			super.visit(n, arg);
-		}
-	}
-	
+
+
 }
