1926976fbcf355f2a3835f1187cc81429d4b3e33
nmsed-iscte <nmsed@iscte-iul.pt>
2021-03-28 16:13:50 +0100

    Testes JUnits, ainda só fiz NewRuleTest e ainda falta testar o main

---
+++ maisTestes/JUnits/maisTestes/LongMethodTest.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package maisTestes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author nmsidique @author henry
+ *
+ */
+class LongMethodTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link maisTestes.LongMethod#fuelArrays()}.
+	 */
+	@Test
+	final void testFuelArrays() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link maisTestes.LongMethod#LongMethod(java.util.ArrayList, java.util.ArrayList, java.util.ArrayList, java.util.ArrayList)}.
+	 */
+	@Test
+	final void testLongMethod() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link maisTestes.LongMethod#main(java.lang.String[])}.
+	 */
+	@Test
+	final void testMain() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
