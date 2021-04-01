c23e1f35afb35fb1e906b2f2c6e7ee43df54810a
luispguerreiro <luis.p.guerreiro@gmail.com>
2021-04-01 23:04:22 +0100

    Cyclo method + WMC class + NOM class finalizados

--- maisTestes/src/Metrics/NOM_Class.java
+++ maisTestes/src/Metrics/NOM_Class.java
@@ -1,4 +1,4 @@
-package maisTestes;
+package Metrics;
 
 import java.util.List;
 
@@ -9,53 +9,21 @@ import com.github.javaparser.ast.body.Parameter;
 
 public class NOM_Class {
 
-	private static final String FILE_PATH = "C:\\Users\\luisg\\Desktop\\SourceCodeParser.java";
+//	private static final String FILE_PATH = "C:\\Users\\luisg\\Desktop\\SourceCodeParser.java";
 	private int numOfMethods = 0;
-	private ClassOrInterfaceDeclaration c;
-
-	// NOM_class (# métodos p/ class) + WMC_Class (complexidade da classe) + CYCLO_method
 
 	public NOM_Class(ClassOrInterfaceDeclaration c) {
-		this.c = c;
 		List<ConstructorDeclaration> contructors = c.getConstructors();
 		List<MethodDeclaration> methods = c.getMethods();
 		numOfMethods += contructors.size() + methods.size();
 		System.out.println("Class " + c.getNameAsExpression() + " tem " + numOfMethods + " métodos");
 
-		if (!contructors.isEmpty() || !methods.isEmpty()) {
-			for (ConstructorDeclaration cs : contructors) {
-				List<Parameter> par = cs.getParameters();
-				String parameters = "";
-				if (!par.isEmpty()) {
-					for (Parameter p : par) {
-//						System.out.println("Nome do parametro: " + p.getType().toString());
-						parameters += p.getType().toString();
-						if (par.size() > 1 && !par.get(par.size() - 1).equals(p)) {
-							parameters += ",";
-						}
-					}
-				}
-				System.out.println("Métodos: " + cs.getNameAsString() + "(" + parameters + ")");
-			}
-			for (MethodDeclaration m : methods) {
-				List<Parameter> par = m.getParameters();
-				String parameters = "";
-				if (!par.isEmpty()) {
-					for (Parameter p : par) {
-//						System.out.println("Nome do parametro: " + p.getType().toString());
-						parameters += p.getType().toString();
-						if (par.size() > 1 && !par.get(par.size() - 1).equals(p)) {
-							parameters += ",";
-						}
-					}
-				}
-				System.out.println("Métodos: " + m.getNameAsString() + "(" + parameters + ")");
-			}
-
-		}
-
 		// System.out.println("Métodos: " + m.getDeclarationAsString(false, false, true));
 	}
+	
+	
+	
+	
 
 //	public static void main(String[] args) {
 //		try {
