9e73017215b7e531f730b2b1e8fc7444ef066727
vmibb-iscte <vmibb@iscte-iul.pt>
2021-04-05 15:39:50 +0100

    JUnit test excel

---
+++ maisTestes/src/maisTestes/ExcelTest.java
@@ -0,0 +1,42 @@
+package maisTestes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ExcelTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	final void testMain() throws FileNotFoundException {
+		Excel excel= new Excel();
+		excel.lerExcel(new File("C:\\Users\\Vasco\\Downloads\\Code_Smells.xlsx"));
+		assertNotNull(excel.getList());
+		assertEquals(247, excel.getList().size());
+		
+//		fail("Not yet implemented");
+	}
+
+}
