b954baeeb92a88d6e14d13b19b52d8009e2a8252
jptfa-iscte <jptfa@iscte-iul.pt>
2021-04-05 15:49:25 +0100

    testes

---
+++ maisTestes/src/maisTestes/LinhaTest.java
@@ -0,0 +1,84 @@
+package maisTestes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LinhaTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSetPacote() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetClasse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetMetodo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetIs_God_Class() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetIs_Long_Method() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPacote() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetClasse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMetodo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetis_God_Class() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetis_Long_Method() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ maisTestes/src/maisTestes/ProcuraTest.java
@@ -0,0 +1,39 @@
+package maisTestes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ProcuraTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetProcura() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLista() {
+		fail("Not yet implemented");
+	}
+
+}
