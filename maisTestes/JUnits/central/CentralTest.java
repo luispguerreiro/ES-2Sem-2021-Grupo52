d97ca5ee13c1e027b96c09a9cef4ed1570174a9b
nmsed-iscte <nmsed@iscte-iul.pt>
2021-04-06 19:50:43 +0100

    CentralTest

---
+++ maisTestes/JUnits/central/CentralTest.java
@@ -0,0 +1,103 @@
+/**
+ * 
+ */
+package central;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.File;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import maisTestes.Excel;
+
+/**
+ * @author nmsid & henry
+ *
+ */
+class CentralTest {
+	static Central c;
+	static File file;
+	static Sheet sheet;
+	static Excel excel;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		c= new Central();
+		file = new File("C:\\Users\\nmsid\\OneDrive\\Ambiente de Trabalho\\jasml_metrics.xlsx");
+	
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link central.Central#Central()}.
+	 */
+	@Test
+	final void testCentral() {
+		Assertions.assertNotNull(c);
+	}
+
+	/**
+	 * Test method for {@link central.Central#writeExcel(org.apache.poi.ss.usermodel.Sheet, org.apache.poi.xssf.usermodel.XSSFWorkbook)}.
+	 */
+	@Test
+	final void testWriteExcel() {
+		
+	}
+
+	/**
+	 * Test method for {@link central.Central#writeClassExcel(int, org.apache.poi.ss.usermodel.Row, org.apache.poi.ss.usermodel.Cell)}.
+	 */
+	@Test
+	final void testWriteClassExcel() {
+		Row cabecalho= sheet.createRow(0);
+		Assertions.assertNotNull(cabecalho);
+	}
+
+	/**
+	 * Test method for {@link central.Central#cabecalho(org.apache.poi.ss.usermodel.Sheet, org.apache.poi.xssf.usermodel.XSSFWorkbook)}.
+	 */
+	@Test
+	final void testCabecalho() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link central.Central#main(java.lang.String[])}.
+	 */
+	@Test
+	final void testMain() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
