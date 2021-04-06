5ae0aeefc235666c73a4cb161a0548dd59bdfa6f
nmsed-iscte <nmsed@iscte-iul.pt>
2021-04-06 18:02:14 +0100

    Source File JUnits

---
+++ maisTestes/JUnits/central/CentralTest.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package central;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author nmsid & henry
+ *
+ */
+class CentralTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link central.Central#Central()}.
+	 */
+	@Test
+	final void testCentral() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link central.Central#writeExcel(org.apache.poi.ss.usermodel.Sheet, org.apache.poi.xssf.usermodel.XSSFWorkbook)}.
+	 */
+	@Test
+	final void testWriteExcel() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link central.Central#writeClassExcel(int, org.apache.poi.ss.usermodel.Row, org.apache.poi.ss.usermodel.Cell)}.
+	 */
+	@Test
+	final void testWriteClassExcel() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link central.Central#cabecalho(org.apache.poi.ss.usermodel.Sheet, org.apache.poi.xssf.usermodel.XSSFWorkbook)}.
+	 */
+	@Test
+	final void testCabecalho() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link central.Central#main(java.lang.String[])}.
+	 */
+	@Test
+	final void testMain() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
