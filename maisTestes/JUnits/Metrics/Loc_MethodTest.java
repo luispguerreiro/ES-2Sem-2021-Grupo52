d9101a9ab5891f0724f4b650f372a72f23d253d0
rfcfa-iscte <rfcfa@iscte-iul.pt>
2021-04-11 12:07:20 +0100

    Testes

---
+++ maisTestes/JUnits/Metrics/Loc_MethodTest.java
@@ -0,0 +1,42 @@
+package Metrics;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Loc_MethodTest {
+	private static String FILE_PATH = "C:\\Users\\r_f_g\\Desktop\\SourceCodeParser.java";
+	static Metrics m;
+	private Loc_Method locMethod2;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	final void testLoc_Method() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	final void testGetResultados() {
+		fail("Not yet implemented");
+	}
+
+}
