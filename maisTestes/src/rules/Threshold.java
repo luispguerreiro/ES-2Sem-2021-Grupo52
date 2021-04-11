e8aaba32c73b6aa085c782fe8a86d1e6b24078c4
hmpms-iscte <hmpms@iscte-iul.pt>
2021-04-11 11:51:16 +0100

    new operators

--- maisTestes/src/rules/Threshold.java
+++ maisTestes/src/rules/Threshold.java
@@ -6,28 +6,27 @@ import java.util.ArrayList;
 
 import Metrics.Resultado;
 import central.BoolResultado;
-import rules.GuiOutput.comparators;
+import rules.Rule.comparator;
 
 public class Threshold implements Serializable {
 
-	private static final String FILE_PATH = "C:\\Users\\henri\\OneDrive\\Ambiente de Trabalho\\ConstantPoolGenerator.java";
-
+//	private static final String FILE_PATH = "C:\\Users\\henri\\OneDrive\\Ambiente de Trabalho\\ConstantPoolGenerator.java";
+	private static String SRC_PATH = "C:\\Users\\nmsid\\Downloads\\jasml_0.10\\src\\com\\jasml\\classes\\ConstantPoolGenerator.java";
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private String metricName;
-	private comparators o;
+	private comparator o;
 	private int limit;
 
 	private ArrayList<Resultado> resultados = new ArrayList<>(); // vai ser ArrayList<Resultado>
 
-	public Threshold(String metricName, comparators o, int limit) throws FileNotFoundException {
+	public Threshold(String metricName, comparator o, int limit) throws FileNotFoundException {
 		this.metricName = metricName;
 		this.o = o;
 		this.limit = limit;
 		System.out.println("Threshold: " + metricName + " " + o + " " + limit);
-
 	}
 
 	public boolean isBigger(int z) throws FileNotFoundException {
@@ -44,42 +43,44 @@ public class Threshold implements Serializable {
 	}
 
 	public boolean result(int z) throws FileNotFoundException {
-		if (o == comparators.BIGGER) {
+		if (o == comparator.BIGGER) {
 			return isBigger(z);
 		}
-		if (o == comparators.EQUALS) {
+		if (o == comparator.EQUALS) {
 			return isEquals(z);
 		}
-		if (o == comparators.SMALLER) {
+		if (o == comparator.SMALLER) {
 			return isSmaller(z);
 		}
 		throw new IllegalStateException();
 	}
-
-	public void setMetricBoolean(ArrayList<BoolResultado> r) {
+	
+	public void setMetricBoolean(ArrayList<BoolResultado> r){
 		int position = positionToGet();
-
 	}
 
-	public int positionToGet() {
-		if (metricName.equals("NOM_class"))
-			return 0;
-		else if (metricName.equals("LOC_class"))
-			return 1;
-		else if (metricName.equals("WMC_class"))
+	
+	public int positionToGet(){
+		if(metricName.equals("NOM_class"))
+				return 0;
+		else if(metricName.equals("LOC_class"))
+				return 1;
+		else if(metricName.equals("WMC_class"))
 			return 2;
-		else if (metricName.equals("LOC_method"))
+		else if(metricName.equals("LOC_method"))
 			return 3;
-		else if (metricName.equals("CYCLO_method"))
+		else if(metricName.equals("CYCLO_method"))
 			return 4;
 		throw new IllegalArgumentException("metric name não encontrado\n");
 	}
+		
+
 
 	public int getLimit() {
 		return limit;
 	}
 
-	public comparators getComparator() {
+	public comparator getComparator() {
 		return o;
 	}
 
@@ -92,7 +93,7 @@ public class Threshold implements Serializable {
 	}
 
 	public static void main(String[] args) throws FileNotFoundException {
-		Threshold t = new Threshold("LOC_method", comparators.SMALLER, 10);
+		Threshold t = new Threshold("LOC_method", comparator.SMALLER, 10);
 
 	}
 
