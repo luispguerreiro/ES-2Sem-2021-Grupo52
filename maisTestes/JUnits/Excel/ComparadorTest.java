f0792107f45d57afb2ae6aeb1cc37cf088812f13
jptfa-iscte <jptfa@iscte-iul.pt>
2021-04-19 19:07:16 +0100

    teste comparador e delete class main (não era usada)

---
+++ maisTestes/JUnits/Excel/ComparadorTest.java
@@ -0,0 +1,79 @@
+package Excel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ComparadorTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testComparador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetExcel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMethodComp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClassComp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCountVP() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCountFP() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCountVN() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCountFN() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetClassCheck() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMethodCheck() {
+		fail("Not yet implemented");
+	}
+
+}
