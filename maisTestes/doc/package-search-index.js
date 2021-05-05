c743a7142bcb6a0b56a99cd269b85b921608d9e6
jptfa-iscte <jptfa@iscte-iul.pt>
2021-05-05 16:13:25 +0100

    jd

--- maisTestes/doc/package-search-index.js
+++ maisTestes/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"GUI"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"central"},{"l":"excel"},{"l":"Excel"},{"l":"GUI"},{"l":"Metrics"},{"l":"rules"}];updateSearchResults();
